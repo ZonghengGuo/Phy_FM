19cc20450fafbf09ce3c601814407c06d829babd
Zongheng Guo <595641630@qq.com>
2025-06-10 12:00:44 +0200

    create env txt

--- saved_model/test.py
+++
@@ -1,8 +0,0 @@
-import torch
-if __name__ == '__main__':
-
-    checkpoint_path = "saved_model/checkpoint-best.pth"
-
-    checkpoint = torch.load(checkpoint_path, map_location=torch.device('cpu'), weights_only=False)
-
-    print(checkpoint.keys())
